8a9fece113df87c0d7444f5575e0a91dfab4ca5f
Yukai Huang <yukaihuang1993@hotmail.com>
2015-10-14 01:56:52 -0700

    use Promise to handle multiple requests
    * 唉這才是該出現的寫法啊，callback 地獄我吃不消也不敢嘗試 QAQ
    * 接下來接 express、接資料庫(也許是 rethinkdb)，然後做個 menutab 或是 chrome extension 的前端 =w=
    * 啊嘶

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
-var cheerio = require('cheerio');
-var request = require('request');
+var cheerio = require("cheerio");
+var Promise = require("bluebird");
+var request = Promise.promisify(require("request"));
 
-request("https://www.ptt.cc/bbs/mobilesales/index.html", function(error, response, html) {
-  if (!error) {
-    var $ = cheerio.load(html);
+var pttRequest = function(kanban, pageCount) {
+  var requestUrl = "https://www.ptt.cc/bbs/" + kanban + "/index" + pageCount + ".html";
+
+  return request(requestUrl).then(function(html) {
+    var $ = cheerio.load(html.toString());
 
     var pageContents = $('.r-ent').map(function(index, element) {
       return {
@@ -17,12 +20,30 @@ request("https://www.ptt.cc/bbs/mobilesales/index.html", function(error, respons
 
     if( $('.r-list-sep').length > 0 ) {
       var titles = $('.r-ent').parent().children().map(function(index, element) {
-        return $(element).find('.title').text().trim()
+        return $(element).find('.title').text().trim();
       }).toArray();
 
       pageContents = pageContents.slice(0, titles.indexOf(""));
     }
 
-    console.log(pageContents);
-  }
+    return pageContents;
+  });
+}
+
+request("https://www.ptt.cc/bbs/mobilesales/index.html").then(function(html) {
+  var $ = cheerio.load(html.toString());
+
+  var lastLastestPage = $('a:contains("‹ 上頁")').attr('href');
+  var currentPageCount = parseInt(/index(\d+)/.exec(lastLastestPage)[1]) + 1;
+
+  var pageCounts = []
+  for(var i = 0; i < 20; i++) { pageCounts.push(currentPageCount - i); }
+
+  Promise.map(pageCounts, function(pageCount) {
+    return pttRequest("mobilesales", pageCount);
+  }).then(function(results) {
+    var articles = [].concat.apply([], results);
+    // TODOs: save it, hash the url, and parse each page in cron jobs or so
+    console.log(articles.length);
+  });
 })
